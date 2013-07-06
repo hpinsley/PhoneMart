0c1905e8257febb7bf1aaf9e722788b8e3e5ceae
Howard Pinsley <hpinsley@gmail.com>
2013-07-06 19:19:43 -0400

    Added basic Angular functionality

---
+++ Angular.Nag.Web/js/controllers/TestController.js
@@ -0,0 +1,7 @@
+﻿function TestController($scope) {
+    $scope.name = "Howard";
+    $scope.phones = [
+        { model: "Nokia 1500", price: 100.0 },
+        { model: "iPhone", price: 80.0 }
+    ];
+}

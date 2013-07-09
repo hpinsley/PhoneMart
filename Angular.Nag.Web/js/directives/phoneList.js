57d5e08da60cb64a3dd53bd15c7f8ea917fd8a15
Howard Pinsley <hpinsley@gmail.com>
2013-07-09 08:59:32 -0400

    Create a phone list directive.

---
+++ Angular.Nag.Web/js/directives/phoneList.js
@@ -0,0 +1,9 @@
+﻿'use strict';
+
+nagApp.directive('phoneList', function () {
+    return {
+        restrict: 'E',
+        templateUrl: '/templates/directives/phoneList.html',
+        replace: true
+    };
+});

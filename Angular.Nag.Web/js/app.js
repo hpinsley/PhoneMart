4e70f501b4291f21dacd3c907dc5c5ea16d32d86
Howard Pinsley <hpinsley@gmail.com>
2013-07-09 08:44:28 -0400

    Got a directive working

---
+++ Angular.Nag.Web/js/app.js
@@ -0,0 +1,3 @@
+﻿'use strict';
+
+var nagApp = angular.module('nagApp', []);
---
+++ Angular.Nag.Web/js/directives/phoneTable.js
@@ -0,0 +1,12 @@
+﻿'use strict';
+
+nagApp.directive('phoneTable', function () {
+    return {
+        restrict: 'E',
+        templateUrl: '/templates/directives/phoneTable.html',
+        replace: true,
+        scope: {
+            phones: "="
+        }
+    };
+});

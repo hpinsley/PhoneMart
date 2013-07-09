a88b761b613e2eed23c4d178f429f88fb6df28ae
Howard Pinsley <hpinsley@gmail.com>
2013-07-09 08:51:22 -0400

    Click event handled properly in directive.  Just inherit scope from the controller.

--- Angular.Nag.Web/js/directives/phoneTable.js
+++ Angular.Nag.Web/js/directives/phoneTable.js
@@ -4,9 +4,6 @@ nagApp.directive('phoneTable', function () {
     return {
         restrict: 'E',
         templateUrl: '/templates/directives/phoneTable.html',
-        replace: true,
-        scope: {
-            phones: "="
-        }
+        replace: true
     };
 });

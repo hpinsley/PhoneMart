168e982215a0448c1e769700d4d45ae479e1c8f6
Howard Pinsley <hpinsley@gmail.com>
2013-07-11 18:18:29 -0400

    Fixed callback click handlers so that I go back to using promise binding.

--- Angular.Nag.Web/js/controllers/PhoneController.js
+++ Angular.Nag.Web/js/controllers/PhoneController.js
@@ -4,22 +4,22 @@ nagApp.controller('PhoneController',
     function PhoneController($scope, phoneData) {
 
         $scope.name = "Howard";
-        var promise = phoneData.getPhones();
-        promise.then(function (phones) {
-            $scope.phones = phones;
-        });
+        $scope.phones = phoneData.getPhones();   //this is a promise that Angular knows how to bind to
 
-        $scope.morePhones = function () {
-            var numPhones = $scope.phones.length + 1;
-            $scope.phones.push({ model: "Phone " + numPhones, price: 10.00 * numPhones });
+        //When you pass the scoped object (which is a promise) back
+        //into a controller method, Angular unwraps the real object for you.
+        
+        $scope.morePhones = function (phones) {
+            var numPhones = phones.length + 1;
+            phones.push({ model: "Phone " + numPhones, price: 10.00 * numPhones });
         };
 
-        $scope.lessPhones = function() {
-            $scope.deletedPhone = $scope.phones.pop();
+        $scope.lessPhones = function(phones) {
+            $scope.deletedPhone = phones.pop();
         };
 
-        $scope.updateDescriptions = function() {
-            $.each($scope.phones, function(i, phone) {
+        $scope.updateDescriptions = function(phones) {
+            $.each(phones, function(i, phone) {
                 phone.description = "Phone " + i;
                 if (phone.price < 100) {
                     phone.description += "\n\nThis phone is under $100.00";

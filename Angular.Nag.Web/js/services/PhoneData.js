669561832c0e38dec310d0ae586c1013cbb898f0
Howard Pinsley <hpinsley@gmail.com>
2013-07-11 08:32:21 -0400

    Phone data comes from a service.

---
+++ Angular.Nag.Web/js/services/PhoneData.js
@@ -0,0 +1,21 @@
+﻿'use strict';
+
+nagApp.factory('phoneData', function ($http, $log) {
+    return {
+        getPhones: function(success) {
+            $http({
+                    method: "GET",
+                    url: "http://localhost/Angular.Nag.Services/api/phones"
+                
+                })
+                .success(function(data, status, headers, config) {
+                    success(data);
+                })
+                .error(function (data, status, headers, config) {
+                    $log.warn(data, status, headers, config);
+                });
+
+
+        }
+    };
+});

56c2d88d0cfce03e4b05e82ad7e9e3436c5834b6
Howard Pinsley <hpinsley@gmail.com>
2013-07-15 08:56:01 -0400

    Introduced routing

--- Angular.Nag.Web/js/app.js
+++ Angular.Nag.Web/js/app.js
@@ -1,3 +1,18 @@
 ﻿'use strict';
 
 var nagApp = angular.module('nagApp', ['ngResource']);
+
+nagApp.config(function ($routeProvider) {
+    $routeProvider
+        .when('/phones',
+            {
+                controller: 'PhoneController',
+                templateUrl: 'templates/phones.html'
+            })
+        .when('/plans',
+            {
+                controller: 'PlansController',
+                templateUrl: 'templates/plans.html'
+            })
+        .otherwise({ redirectTo: '/phones' });
+});

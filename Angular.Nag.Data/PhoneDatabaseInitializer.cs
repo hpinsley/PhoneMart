b78b1c3ed07b333509724921989d2ef1a1c58a8b
Howard Pinsley <hpinsley@gmail.com>
2013-07-30 09:14:36 -0400

    Changed DataMinutes to DataMegabytes.  Added a few more phones to the initializer.

--- Angular.Nag.Data/PhoneDatabaseInitializer.cs
+++ Angular.Nag.Data/PhoneDatabaseInitializer.cs
@@ -23,22 +23,30 @@ namespace Angular.Nag.Data {
             context.Manufacturers.Add(google);
             context.Manufacturers.Add(microsoft);
 
-            var phone1 = new Phone { Manufacturer = nokia, Model = "Global 1000", Price = 25.0m, Description = @"Capture more of the moment with a 41 megapixel camera sensor, Full HD video and Nokia Rich Recording for incredible sound quality.", ImageFile = "phone01.jpg" };
-            var phone2 = new Phone { Manufacturer = google, Model = "Android 200", Price = 75.0m, Description = @"Your home screen. Your world. With HTC BlinkFeed™ on your phone, you’re never out of touch with your world. All your favorite content is streamed live onto one screen. If it’s happening now, you’ll find it on your home screen.  Awesome camera. Awesome imaging tools.Awesome camera. Awesome imaging tools. Get perfect images with one-press continuous shooting, VideoPic, and a camera that captures 300% more light.Slim phone. Fat sound.  Slim phone. Dual frontal stereo speakers are teamed with powerful amplifiers, so everyone can hear what you’re hearing. Share music, share videos, share games—and share them loudly.", ImageFile = "phone02.jpg" };
-            var phone3 = new Phone { Manufacturer = apple, Model = "iPhone 4", Price = 175.0m, Description = @"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).", ImageFile = "phone03.png" };
-            var phone4 = new Phone { Manufacturer = apple, Model = "iPhone 5",Price = 50.0m,Description = @"Apples's new top offering.  The iPhone 5 comes with Siri and a wealth of productivity apps making it the choice of todays savvy consumer.",ImageFile = "phone04.png"};
+            var global1000 = new Phone { Manufacturer = nokia, Model = "Global 1000", Price = 25.0m, Description = @"Capture more of the moment with a 41 megapixel camera sensor, Full HD video and Nokia Rich Recording for incredible sound quality.", ImageFile = "phone01.jpg" };
+            var android200 = new Phone { Manufacturer = google, Model = "Android 200", Price = 75.0m, Description = @"Your home screen. Your world. With HTC BlinkFeed on your phone, you’re never out of touch with your world. All your favorite content is streamed live onto one screen. If it’s happening now, you’ll find it on your home screen.", ImageFile = "phone02.jpg" };
+            var iPhone4 = new Phone { Manufacturer = apple, Model = "iPhone 4", Price = 175.0m, Description = @"The iPhone 4 is Apple's upgrade on the revolutionary iPhone.  It features the touch-screen user interface that has set the standard in consumer smart phones.", ImageFile = "phone03.png" };
+            var iPhone5 = new Phone { Manufacturer = apple, Model = "iPhone 5", Price = 50.0m, Description = @"Apples's new top offering.  The iPhone 5 comes with Siri and a wealth of productivity apps making it the choice of todays savvy consumer.", ImageFile = "phone04.png" };
+            var galaxy = new Phone { Manufacturer = samsung, Model = "Galaxy", Price = 150.0m, Description = @"The wildly popular Galaxy features its iconic large screen and a wealth of apps from the legions of Android developers", ImageFile = "phone02.jpg" };
+            var win8 = new Phone { Manufacturer = microsoft, Model = "Win 8", Price = 125.0m, Description = @"Microsoft's Win 8 phone is its first entry into the smart phone market that is based on its new Metro-style user interface", ImageFile = "phone01.jpg" };
+            var lgSlim = new Phone { Manufacturer = lg, Model = "Slimline", Price = 75.0m, Description = @"The original flip phone with its simple-to-use interface.", ImageFile = "phone01.jpg" };
+            var starLight = new Phone { Manufacturer = casio, Model = "Star Light", Price = 175.0m, Description = @"Casio's top of the line phone.", ImageFile = "phone02.jpg" };
 
-            context.Phones.Add(phone1);
-            context.Phones.Add(phone2);
-            context.Phones.Add(phone3);
-            context.Phones.Add(phone4);
+            context.Phones.Add(global1000);
+            context.Phones.Add(android200);
+            context.Phones.Add(iPhone4);
+            context.Phones.Add(iPhone5);
+            context.Phones.Add(galaxy);
+            context.Phones.Add(win8);
+            context.Phones.Add(lgSlim);
+            context.Phones.Add(starLight);
 
             context.SaveChanges();
 
-            var plan1 = new Plan { PlanName = "Family Text and Talk", MonthlyCost = 100.0m, DataMinutes = 1000, VoiceMinutes = 500, Phones = new List<Phone> { phone1, phone3 } };
-            var plan2 = new Plan { PlanName = "Yappers and Texters", MonthlyCost = 50.0m, DataMinutes = 500, VoiceMinutes = 250, Phones = new List<Phone> { phone1, phone3, phone4 } };
-            var plan3 = new Plan { PlanName = "Teenage Text Special", MonthlyCost = 50.0m, DataMinutes = 500, VoiceMinutes = 250, Phones = new List<Phone> { phone2, phone3, phone4 } };
-            var plan4 = new Plan { PlanName = "Frugal Family", MonthlyCost = 50.0m, DataMinutes = 500, VoiceMinutes = 250, Phones = new List<Phone> { phone1, phone3 } };
+            var plan1 = new Plan { PlanName = "Family Text and Talk", MonthlyCost = 100.0m, DataMegabytes = 1000, VoiceMinutes = 500, Phones = new List<Phone> { global1000, iPhone4, galaxy, win8 } };
+            var plan2 = new Plan { PlanName = "Yappers and Texters", MonthlyCost = 50.0m, DataMegabytes = 500, VoiceMinutes = 250, Phones = new List<Phone> { global1000, iPhone4, iPhone5, starLight } };
+            var plan3 = new Plan { PlanName = "Teenage Text Special", MonthlyCost = 50.0m, DataMegabytes = 1000, VoiceMinutes = 150, Phones = new List<Phone> { android200, iPhone4, iPhone5, galaxy, starLight } };
+            var plan4 = new Plan { PlanName = "Frugal Family", MonthlyCost = 25.0m, DataMegabytes = 250, VoiceMinutes = 250, Phones = new List<Phone> { global1000, iPhone4, win8, lgSlim } };
 
             context.Plans.Add(plan1);
             context.Plans.Add(plan2);
@@ -55,10 +63,10 @@ namespace Angular.Nag.Data {
 
             context.SaveChanges();
 
-            var howardPhone1 = new PhoneInstance { Phone = phone1, PhonePlan = plan1, SerialNumber = "1XGY765-FDRFG-34JFKEK", PhoneNumber = "111-424-0430" };
-            var howardPhone2 = new PhoneInstance { Phone = phone2, PhonePlan = plan2, SerialNumber = "2XGY765-FDRFG-34JFKEK", PhoneNumber = "222-424-0430" };
-            var davidPhone1 = new PhoneInstance { Phone = phone3, PhonePlan = plan3, SerialNumber = "3XGY765-FDRFG-34JFKEK", PhoneNumber = "333-424-0430" };
-            var davidPhone2 = new PhoneInstance { Phone = phone4, PhonePlan = plan4, SerialNumber = "4XGY765-FDRFG-34JFKEK", PhoneNumber = "444-424-0430" };
+            var howardPhone1 = new PhoneInstance { Phone = global1000, PhonePlan = plan1, SerialNumber = "1XGY765-FDRFG-34JFKEK", PhoneNumber = "111-424-0430" };
+            var howardPhone2 = new PhoneInstance { Phone = android200, PhonePlan = plan2, SerialNumber = "2XGY765-FDRFG-34JFKEK", PhoneNumber = "222-424-0430" };
+            var davidPhone1 = new PhoneInstance { Phone = iPhone4, PhonePlan = plan3, SerialNumber = "3XGY765-FDRFG-34JFKEK", PhoneNumber = "333-424-0430" };
+            var davidPhone2 = new PhoneInstance { Phone = iPhone5, PhonePlan = plan4, SerialNumber = "4XGY765-FDRFG-34JFKEK", PhoneNumber = "444-424-0430" };
 
             context.PhoneInstances.Add(howardPhone1);
             context.PhoneInstances.Add(howardPhone2);
--- Angular.Nag.Models/Plan.cs
+++ Angular.Nag.Models/Plan.cs
@@ -6,7 +6,7 @@ namespace Angular.Nag.Models {
         public string PlanName { get; set; }
         public decimal MonthlyCost { get; set; }
         public int VoiceMinutes { get; set; }
-        public int DataMinutes { get; set; }
+        public int DataMegabytes { get; set; }
 
         public List<Phone> Phones { get; set; }
 

37e566a05e52182711c3eebb711613cdef362211
Howard Pinsley <hpinsley@gmail.com>
2013-07-31 19:26:00 -0400

    Clean up the form a bit allowing a navigation link back to the parent account.

--- Angular.Nag.Web/js/controllers/PhoneInstanceLookupController.js
+++ Angular.Nag.Web/js/controllers/PhoneInstanceLookupController.js
@@ -6,6 +6,13 @@ nagApp.controller('PhoneInstanceLookupController',
         $scope.phoneInstanceId = $routeParams.phoneInstanceId;
         $scope.plans = phoneData.getPlans();
         $scope.account = phoneData.getAccount($routeParams.accountId);
+
+        //We want the form controls to bind to independent scope variables -- 
+        //i.e. not directly to the values in the returned phoneInstance.
+        //Since we get the phone instance with a resource.get call, we have
+        //to utilitize it's callback so as to set the independent variables
+        //only once we have the data.
+        
         $scope.phoneInstance = phoneData.getPhoneInstance($routeParams.accountId, $routeParams.phoneInstanceId,
             function (phoneInstance) {
                 $scope.serialNumber = phoneInstance.serialNumber;
@@ -14,7 +21,4 @@ nagApp.controller('PhoneInstanceLookupController',
             }
         );
 
-        $scope.planSelected = function() {
-            console.log("You selected " + $scope.phonePlanId);
-        };
     });

5828cb7fb6e5627300952542cb2be559d8b30ef8
Howard Pinsley <hpinsley@gmail.com>
2013-08-06 08:55:59 -0400

    Added a phone number filter to standardize US 10-digit numbers.

---
+++ Angular.Nag.Web/js/filters/phoneNumberFilter.js
@@ -0,0 +1,19 @@
+﻿'use strict';
+
+nagApp.filter('phoneNumber', function () {
+    var phoneNumberFilter = function(input) {
+        var letters = input.split("");
+        var digits = _.filter(letters, function (c) {
+            return c >= "0" && c <= "9";
+        });
+        var rawDigits = digits.join("");
+        if (rawDigits.length == 10)
+            return "("  + rawDigits.slice(0, 3) + ") "
+                        + rawDigits.slice(3, 6) + "-"
+                        + rawDigits.slice(6, 10)
+        else {
+            return input;   //only format standard US numbers
+        }
+    };
+    return phoneNumberFilter;
+});

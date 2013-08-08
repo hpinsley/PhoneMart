55673b9010cb9ccd57ac13f1edcbf5b9b2230405
Howard Pinsley <hpinsley@gmail.com>
2013-08-08 09:35:56 -0400

    Setting up for Angular testing.

---
+++ Angular.Nag.Web/tests/PlansControllerSpec.js
@@ -0,0 +1,19 @@
+﻿describe("PlansController", function () {
+
+    var $scope, ctrl;
+
+    beforeEach(module('nagApp'));
+
+    beforeEach(inject(function ($rootScope, $controller) {
+        $scope = $rootScope.$new();
+        ctrl = $controller("PlansController", { $scope: $scope });
+    }));
+
+    it("should setup controller properly", function () {
+        expect(true).toBe(true);
+        expect(nagApp).toBeDefined();
+        expect($scope).toBeDefined();
+        expect(ctrl).toBeDefined();
+    });
+
+});

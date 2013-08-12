ea0ca5921c4e56c921e1d3b714c79ea6df463ab8
Howard Pinsley <hpinsley@gmail.com>
2013-08-12 08:31:24 -0400

    PlansControllerSpec.js contains two passing tests for PlansController.  Two different approaches are used.

--- Angular.Nag.Web/tests/PlansControllerSpec.js
+++ Angular.Nag.Web/tests/PlansControllerSpec.js
@@ -6,27 +6,48 @@ describe("PlansController", function () {
     var scope;
     var mockPhoneData;
     var location;
-
+    var httpMock;
+    var phoneDataSvc;
+    
     beforeEach(module('nagApp'));
 
-    beforeEach(inject(function ($controller, $rootScope, $location) {
+    beforeEach(inject(function ($controller, $rootScope, $location, $httpBackend, phoneData) {
         $controllerConstructor = $controller;
         scope = $rootScope.$new();
         location = $location;
+        httpMock = $httpBackend;
+        phoneDataSvc = phoneData;
+        
         mockPhoneData = sinon.stub(
             { getPlans: function () {}}
         );
     }));
 
-    it("plansController should return obtain phone plans and set it on the scope", function() {
+    it("should return obtain phone plans and set it on the scope", function() {
 
-        var mockPlans = {dummy: 1};
+        var mockPlans = {};
         mockPhoneData.getPlans.returns(mockPlans);
 
         var ctrl = $controllerConstructor('PlansController',
             { $scope: scope, $location: location, phoneData: mockPhoneData });
 
         expect(scope.plans).toBe(mockPlans);
+    });
+
+    it("should return obtain phone plans using http", function () {
+
+        var mockPlans = {};
+        httpMock.when("GET", nagApp.getServicesRoot() + "/api/plans").respond(mockPlans);
+
+        var ctrl = $controllerConstructor('PlansController',
+            { $scope: scope, $location: location, phoneData: phoneDataSvc });
+
+        expect(scope.plans).toBeDefined();
+        expect(scope.plans.then).toBeDefined();
+        scope.plans.then(function (plans) {
+            expect(plans).toBe(mockPlans);
+        });
+        httpMock.flush();
     });
 
 });

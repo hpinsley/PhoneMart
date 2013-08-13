79782234546da9015e5ce3f445e7f02a2a98cd13
Howard Pinsley <hpinsley@gmail.com>
2013-08-13 18:50:39 -0400

    Added an AccountsControllerSpec file.

---
+++ Angular.Nag.Web/tests/AccountsControllerSpec.js
@@ -0,0 +1,41 @@
+﻿'use strict';
+
+describe("AccountsController", function () {
+
+    var $controllerConstructor;
+    var scope;
+    var mockPhoneData;
+    var location;
+    var httpMock;
+    var phoneDataSvc;
+    var q;
+    var ctrl;
+    var accountList = [{ accountId: 1 }];
+    
+    beforeEach(module('nagApp'));
+
+    beforeEach(inject(function ($controller, $rootScope, $location, $httpBackend, phoneData, $q) {
+        $controllerConstructor = $controller;
+        scope = $rootScope.$new();
+        location = $location;
+        httpMock = $httpBackend;
+        phoneDataSvc = phoneData;
+        q = $q;
+                
+        mockPhoneData = sinon.stub(
+            {
+                getAccounts: function () {}
+            }
+        );
+
+        mockPhoneData.getAccounts.returns(accountList);
+        ctrl = $controllerConstructor('AccountsController',
+            { $scope: scope, $location: location, phoneData: mockPhoneData});
+
+    }));
+
+    it('should set the list of accounts on the scope', function () {
+        expect(scope.accounts).toBeDefined();
+        expect(scope.accounts).toBe(accountList);
+    });
+});

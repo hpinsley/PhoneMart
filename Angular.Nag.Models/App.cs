c07cb784fc2f44dfe82aa71455db65ab13f6ac29
Howard Pinsley <hpinsley@gmail.com>
2013-08-22 18:39:16 -0400

    Added Glimpse and a model file for the future app class.  I'm going to develop it on a separate branch.  After this commit to master, I'll createe the app branch.

---
+++ Angular.Nag.Models/App.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Angular.Nag.Models {
+    public class App {
+        public int AppId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public override string ToString() {
+            return string.Format("App: Id: {0}, Name: {1}", AppId, Name);
+        }
+    }
+}
